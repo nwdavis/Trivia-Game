eaa5f3688a3238b974897b264997549b0034e99e
nwdavis <nathanieldavis89@gmail.com>
2017-06-26 13:51:38 -0400

    css and questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//timer
+//questions
+//what happens when user clicks start (entire game wrapped in a function)
+//what happens when user gets it right
+//what happens when user gets it wrong
+//reset game
+
+var questions = ["The St. Lawrence River forms part of the border between which two countries?",
+	"What U.S. National Park, located in the Northwest corner of Montana, has the nickname 'Crown of the Continent?'",
+	"Sardinia, the second largest island in the Mediterranean Sea, is an autonomous region of what country?",
+	"Beirut is the capital and largest city of what country?",
+	"Located in Northwestern Turkey, which strait separates Europe and Asia?",
+	"South Africa completely surrounds which other African nation?",
+	"The Yangtze River is entirely located in which country?",
+	"How many US states border the Pacific Ocean?",
+	]
+
+var 
