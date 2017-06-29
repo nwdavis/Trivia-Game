954db486b9ee1db89f0b6fb35766c153dcd270c7
nwdavis <nathanieldavis89@gmail.com>
2017-06-28 23:12:03 -0400

    sort of working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,14 +49,27 @@ var questions = [
 	correctAnswer: 3,
 }];
 
+var facts = [
+	"The St. Lawrence River drains into the Gulf of St. Lawrence, the largest estuary in the world.",
+	"Glacier National Park was established in 1910, and has 3 million visitors per year, on average.",
+	"Sardines are named after the island of Sardinia, where they were once found in abundance.",
+	"Beirut is also known as the Paris of the East.",
+	"There are three bridges that cross the Bosphorus, the most recent of which opened to traffic in 2016.",
+	"A mountainous country, Lesotho (pronounced li-SOO-too) is the southernmost landlocked country in the world.",
+	"The Yangtze River is the third largest river in the world.",
+	"While only 5 states border the Pacific, there are numerous US Overseas Territories in the Pacific Ocean.",
+]
+
 var currentQuestion = 0;
+
+var userAnswer = 0;
 
 var rightAnswers = 0;
 
 var wrongAnswers = 0;
 
 var timer = {
-	value: 5,
+	value: 20,
 	running: false,
 };
 
@@ -65,20 +78,65 @@ $("#gamePlay").hide();
 
 	function game() {
 
+		function gameResetTimer () {
+			var intervalId = setInterval(decrement, 1000);
+			var x = 10;
+
+			function decrement() {
+				x--;
+				if (x === 0) {
+					currentQuestion = 0;
+					rightAnswers = 0;
+					wrongAnswers = 0;
+					displayQuestion();
+				};
+			};
+		};
+
+		function nextQuestion() {
+
+			var intervalId = setInterval(increment, 1000);
+
+			var x = 0;
+
+			function increment () {
+				x++;
+				if (x === 10) {
+					displayQuestion();
+				};
+			};
+
+			if (currentQuestion === 8) {
+				$("#answerOptions").hide();
+				$("#questionHeader").html("<h2>Thanks for playing!</h2>")
+				$("#questionContent").html("<h3>Correct: " + rightAnswers + "</h3><br><h3>Incorrect: " + wrongAnswers + 
+					"</h3>");
+				gameResetTimer();	
+			};
+
+		};
+
 		function displayQuestion() {
 
 			var i = currentQuestion;
+
+			if (i !== 0) {
+				console.log("not equal to 0")
+			}
 
 			function timerStart () {
 
 		    	timer.running = true;
 
-		    	intervalId = setInterval(decrement, 1000);
+		    	var intervalId = setInterval(decrement, 1000);
 
 		    	function decrement() {
 		    		timer.value--;
 		    		$("#counterDiv").html("<h2>" + timer.value + "</h2>")
 
+		    		if (timer.running === false) {
+		    			clearInterval(intervalId);
+		    		}
 		    		if (timer.value === 0) {
 
 						$("#answerOptions").hide();
@@ -101,14 +159,62 @@ $("#gamePlay").hide();
 			$("#optionD").html("<h3>" + questions[i].options[3] + "<h3>");
 
 			timerStart();
+
+			$("#optionA").on("click", function() {
+				userAnswer = 0;
+				if (userAnswer === questions[currentQuestion].correctAnswer) {
+					rightAnswers++;
+					$("#questionHeader").html("<h2>Correct!</h2>");
+					$("#questionContent").html("<h3>" + facts[i] + "</h3>");
+					currentQuestion++;
+					timer.value = 20;
+					timer.running = false;
+					nextQuestion();
+				};
+			});
+			$("#optionB").on("click", function() {
+				userAnswer = 1;
+				if (userAnswer === questions[currentQuestion].correctAnswer) {
+					rightAnswers++;
+					$("#questionHeader").html("<h2>Correct!</h2>");
+					$("#questionContent").html("<h3>" + facts[i] + "</h3>");
+					currentQuestion++;
+					timer.value = 20;
+					timer.running = false;
+					nextQuestion();
+				};
+			});
+			$("#optionC").on("click", function() {
+				userAnswer = 2;
+				if (userAnswer === questions[currentQuestion].correctAnswer) {
+					rightAnswers++;
+					$("#questionHeader").html("<h2>Correct!</h2>");
+					$("#questionContent").html("<h3>" + facts[i] + "</h3>");
+					currentQuestion++;
+					timer.value = 20;
+					timer.running = false;
+					nextQuestion();
+				};
+			});
+			$("#optionD").on("click", function() {
+				userAnswer = 3;
+				if (userAnswer === questions[currentQuestion].correctAnswer) {
+					rightAnswers++;
+					$("#questionHeader").html("<h2>Correct!</h2>");
+					$("#questionContent").html("<h3>" + facts[i] + "</h3>");
+					currentQuestion++;
+					timer.value = 20;
+					timer.running = false;
+					nextQuestion();
+				};
+			});
 		};
+
 
 
 
 	$("#gamePlay").show("fast", function(){});
 		displayQuestion();
-
-
 	};
 
 $("#startButton").on("click", function(){
